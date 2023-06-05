d428c2178445f21d9fec096ac02ac7bf06603dd6
Juan Manuel De La Torre Sanchez <jdela@unal.edu.co>
2023-06-05 00:13:07 -0500

    grammar and all gen files

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/test')
+def test():
+  return 'Hola pene'
+
 if __name__ == '__main__':
   app.run(port=5000)
